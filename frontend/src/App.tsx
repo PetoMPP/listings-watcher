f4a33a12e0da049c3e1eac09c639371734c1ef32
PetoMPP <88336224+PetoMPP@users.noreply.github.com>
2025-06-21 12:03:30 +0200

    Add TailwindCSS and FlyonUI, move app root to body, add search mockup removing default template.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-96">
+      <h1 className="font-semibold text-3xl pb-4">Listings watcher</h1>
+      <div className="join max-w-sm">
+        <input className="input join-item" placeholder="Search" />
+        <button className="btn btn-outline btn-primary join-item">Search</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
